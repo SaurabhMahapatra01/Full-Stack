f380c8981b1389eae0cfb87939e0975da3c738d2
Saurabh_01 <saurabhmahapatra001@gmail.com>
2023-01-14 12:03:40 +0530

    1 FirstClass JS

---
+++ JavaScript/first.js
@@ -0,0 +1,31 @@
+// const uid = "sam001"
+
+// var fullName = "Saurabh Mahapatra"
+// var Email = "saurabhmahapatra001@gmail.com"
+// var MobNo = 7768917057
+// var password = "Saurbh01@"
+// var confirpassword = "Saurabh01@"
+
+// console.log(`
+
+//  The Unique id is : ${uid}
+//  of ${fullName} and his email id is ${Email}
+//  & Mobile number${MobNo} and Last but not least the password ${password} 
+// `);
+
+
+// console.log(true - true);
+// console.log(8 + "2");
+// console.log(8 - "3");
+
+// confirm("hlw mitranoo");
+// alert("niceeeeeeeeeeee")
+// document.write("oohk ohk theekkkkkkkkk");
+// prompt("hahaa bhai", "saurabh");
+
+
+// var hh = document.getElementById("hz");
+// console.log(hh);
+// document.getElementById("hz").innerHTML = "<h1>Hlw Guys</h1>";
+
+ var h = document.getElementByClassName("m")[0].innerHTML="are baapre";
